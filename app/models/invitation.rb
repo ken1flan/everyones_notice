1a2e94881e79204756c4231ad3a1b6ab696cd1ba
ken1flan <ken1flan@gmail.com>
2014-10-26 13:48:53 +0900

    refs #4 トークン作成メソッド

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,2 +1,10 @@
 class Invitation < ActiveRecord::Base
+  EXPIRATION_PERIOD = 3.days
+
+  def generate_token
+    begin
+      self.token = SecureRandom.urlsafe_base64
+    end while Invitation.exists?(token: self.token)
+    self.expired_at = EXPIRATION_PERIOD.ago
+  end
 end
--- test/models/invitation_test.rb
+++ test/models/invitation_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
-class InvitationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Invitation do
+  before { Timecop.freeze }
+  after { Timecop.return }
+
+  describe "#generate_token" do
+    context "実行したとき" do
+      before do
+        @invitation = build(:invitation)
+        @invitation.generate_token
+      end
+
+      it "tokenが設定されていること" do
+        @invitation.present?.must_equal true
+      end
+
+      it "expired_atが#{Invitation::EXPIRATION_PERIOD.ago}に設定されていること" do
+        @invitation.expired_at.must_equal Invitation::EXPIRATION_PERIOD.ago
+      end
+    end
+  end
 end
