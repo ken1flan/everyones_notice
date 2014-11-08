fa5bd9955c2110d9412eb9fd7950398459457620
ken1flan <ken1flan@gmail.com>
2014-11-08 19:17:36 +0900

    ワンワン対応

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -3,13 +3,13 @@ def set_auth_mock( provider, uid, nickname )
 end
 
 def create_user_and_identity(provider)
- user = create(:user)
- identity = create(:identity, user_id: user.id, provider: provider)
- user
+  user = create(:user)
+  create(:identity, user_id: user.id, provider: provider)
+  user
 end
 
 def login
   user = create_user_and_identity("twitter")
-  set_auth_mock( "twitter", user.identities.first.uid, user.nickname )
+  set_auth_mock("twitter", user.identities.first.uid, user.nickname)
   visit "/auth/twitter"
 end
