e4d10cc04151606ffaa4f880b369ca1994353206
ken1flan <ken1flan@gmail.com>
2014-11-15 20:07:36 +0900

    refs #23 アノテート

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: replies
+#
+#  id         :integer          not null, primary key
+#  notice_id  :integer          not null
+#  body       :text             not null
+#  user_id    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#
+# Indexes
+#
+#  index_replies_on_notice_id  (notice_id)
+#  index_replies_on_user_id    (user_id)
+#
+
 class Reply < ActiveRecord::Base
   belongs_to :user
   belongs_to :notice
