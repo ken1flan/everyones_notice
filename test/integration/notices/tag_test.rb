917eeb54d101ce0e201f969ee484afba0458ec27
ken1flan <ken1flan@gmail.com>
2015-02-11 23:02:25 +0900

    refs #45 ほかのひとがきづきにタグをつける

---
+++ test/integration/notices/tag_test.rb
@@ -0,0 +1,65 @@
+require "test_helper"
+
+describe "きづきのタグ Integration" do
+  before do
+    @user = login
+    @notice = create(:notice)
+  end
+
+  context "きづき一覧を訪れたとき" do
+    before { visit notices_path }
+
+    context "タグの追加ボタンを押したとき" do
+      before { click_button "add_tag_button_#{@notice.id}" }
+
+      context "タグ名を入れないで保存したとき" do
+        before do
+          click_button "submit_notice_tag_button_#{@notice.id}"
+          sleep 1 # 更新に時間がかかるため
+        end
+
+        it "エラーが表示されること" do
+          page.text.must_include "タグ名を入力してください"
+        end
+      end
+
+      context "今まで登録されていないタグ名を入れて保存したとき" do
+        before do
+          fill_in "notice_tag_name_#{@notice.id}", with: "タグ1"
+          click_button "submit_notice_tag_button_#{@notice.id}"
+          sleep 1 # 更新に時間がかかるため
+        end
+
+        it "タグ名が表示されていること" do
+          page.text.must_include "タグ1"
+        end
+
+        context "タグ一覧ページを訪れたとき" do
+          before { visit tags_path }
+
+          it "タグ名が表示されていること" do
+            page.text.must_include "タグ1"
+          end
+        end
+      end
+
+      context "すでに登録されているタグ名を入れて保存したとき" do
+        before do
+          @tag = create(:tag)
+          @tags_count = Tag.count
+          fill_in "notice_tag_name_#{@notice.id}", with: @tag.name
+          click_button "submit_notice_tag_button_#{@notice.id}"
+          sleep 1 # 更新に時間がかかるため
+        end
+
+        it "タグ名が表示されていること" do
+          page.text.must_include @tag.name
+        end
+
+        it "タグ数は増えていないこと" do
+          Tag.count.must_equal @tags_count
+        end
+      end
+    end
+  end
+end
--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -10,8 +10,32 @@
 
 require 'test_helper'
 
-class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Tag do
+  describe "バリデーション" do
+    before { @tag = build(:tag) }
+
+    describe "name" do
+      valid_data = [1, 2, "a", "あ", "あ"*32]
+      valid_data.each do |vd|
+        context "title = #{vd}のとき" do
+          before { @tag.name = vd }
+
+          it "validであること" do
+            @tag.valid?.must_equal true
+          end
+        end
+      end
+
+      invalid_data = [nil, "", "あ,あ", "あ"*33]
+      invalid_data.each do |ivd|
+        context "title = #{ivd}のとき" do
+          before { @tag.name = ivd }
+
+          it "invalidであること" do
+            @tag.invalid?.must_equal true
+          end
+        end
+      end
+    end
+  end
 end
