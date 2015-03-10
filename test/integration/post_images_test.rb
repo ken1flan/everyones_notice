117c2d6a4adafca16cc96c9bf6a78b81ded95013
ken1flan <ken1flan@gmail.com>
2015-03-11 07:54:36 +0900

    登録のintegration追加

--- test/integration/post_images_test.rb
+++ test/integration/post_images_test.rb
@@ -7,21 +7,44 @@ describe "画像の投稿 Integration" do
     end
 
     context "画像一覧で「新規追加」ボタンを押したとき" do
+      before do
+        visit post_images_path
+        click_link "新規作成"
+      end
+
       context "なにも記入しないで「保存」ボタンを押したとき" do
+        before do
+          click_button "登録する"
+        end
+
         it "エラーが出ること" do
+          page.text.must_include "を入力してください"
         end
 
         context "正しい情報を入力して「保存」ボタンを押したとき" do
-          it "詳細画面に遷移していること" do
+          before do
+            fill_in "post_image_title", with: "画像のタイトル"
+            attach_file "post_image_image", Rails.root.join("test", "fixtures", "images", "テスト画像.jpg")
+            click_button "登録する"
           end
 
           it "一覧画面に表示されていること" do
+            page.text.must_include "画像のタイトル"
+            page.html.must_include "_____.jpg"
           end
         end
       end
 
       context "正しい情報を入力して「保存」ボタンを押したとき" do
-        it "詳細画面に遷移していること" do
+        before do
+          fill_in "post_image_title", with: "画像のタイトル"
+          attach_file "post_image_image", Rails.root.join("test", "fixtures", "images", "テスト画像.jpg")
+          click_button "登録する"
+        end
+
+        it "一覧画面に表示されていること" do
+          page.text.must_include "画像のタイトル"
+          page.html.must_include "_____.jpg"
         end
       end
     end
