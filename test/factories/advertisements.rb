c2a3a8fab34724b806b0da58216896d354b4b60d
ken1flan <ken1flan@gmail.com>
2015-03-16 19:51:44 +0900

    アノテーション

--- test/factories/advertisements.rb
+++ test/factories/advertisements.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: advertisements
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  summary    :string           not null
+#  body       :text             not null
+#  started_on :date             not null
+#  ended_on   :date             not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_advertisements_on_started_on_and_ended_on  (started_on,ended_on)
+#  index_advertisements_on_updated_at               (updated_at)
+#
+
 FactoryGirl.define do
   factory :advertisement do
     title { "title" + ('a'..'z').to_a.sample(26).join }
