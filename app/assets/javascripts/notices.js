19143fa12f485f44fafac0b7acfda419a9ab4064
ken1flan <ken1flan@gmail.com>
2015-05-02 17:00:05 +0900

    きづきでプレビューを使えるようにした

--- app/assets/javascripts/notices.js
+++ app/assets/javascripts/notices.js
@@ -11,16 +11,16 @@ function set_notice_writing_preview () {
   if($(this).hasClass("notice_writing_tab")) {
     $(this).addClass("active");
     $(this).parent().children(".notice_preview_tab").removeClass("active");
-    $(this).parent().parent().children(".notice_writing").removeClass("sr-only");
-    $(this).parent().parent().children(".notice_preview").addClass("sr-only");
+    notice_writing.removeClass("sr-only");
+    notice_preview.addClass("sr-only");
   } else {
     $(this).addClass("active");
     $(this).parent().children(".notice_writing_tab").removeClass("active");
-    $(this).parent().parent().children(".notice_writing").addClass("sr-only");
-    $(this).parent().parent().children(".notice_preview").removeClass("sr-only");
+    notice_writing.addClass("sr-only");
+    notice_preview.removeClass("sr-only");
     $.get("/utils/markdown", { src: notice_writing.val() }, function (data) {
 
-      notice_preview.children(".panel-body").html(data);
+      notice_preview.find(".markdown_body").html(data);
     });
   }
 }
