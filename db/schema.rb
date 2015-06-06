83ce86b664bf5e68397c37ca2ae42e3bd0baaf08
ken1flan <ken1flan@gmail.com>
2015-06-07 03:04:02 +0900

    テーブルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -163,52 +163,6 @@ ActiveRecord::Schema.define(version: 20150526164516) do
   add_index "replies", ["notice_id"], name: "index_replies_on_notice_id"
   add_index "replies", ["user_id"], name: "index_replies_on_user_id"
 
-  create_table "rs_evaluations", force: :cascade do |t|
-    t.string   "reputation_name", limit: 255
-    t.integer  "source_id"
-    t.string   "source_type",     limit: 255
-    t.integer  "target_id"
-    t.string   "target_type",     limit: 255
-    t.float    "value",                       default: 0.0
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "data"
-  end
-
-  add_index "rs_evaluations", ["reputation_name", "source_id", "source_type", "target_id", "target_type"], name: "index_rs_evaluations_on_reputation_name_and_source_and_target", unique: true
-  add_index "rs_evaluations", ["reputation_name"], name: "index_rs_evaluations_on_reputation_name"
-  add_index "rs_evaluations", ["source_id", "source_type"], name: "index_rs_evaluations_on_source_id_and_source_type"
-  add_index "rs_evaluations", ["target_id", "target_type"], name: "index_rs_evaluations_on_target_id_and_target_type"
-
-  create_table "rs_reputation_messages", force: :cascade do |t|
-    t.integer  "sender_id"
-    t.string   "sender_type", limit: 255
-    t.integer  "receiver_id"
-    t.float    "weight",                  default: 1.0
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "rs_reputation_messages", ["receiver_id", "sender_id", "sender_type"], name: "index_rs_reputation_messages_on_receiver_id_and_sender", unique: true
-  add_index "rs_reputation_messages", ["receiver_id"], name: "index_rs_reputation_messages_on_receiver_id"
-  add_index "rs_reputation_messages", ["sender_id", "sender_type"], name: "index_rs_reputation_messages_on_sender_id_and_sender_type"
-
-  create_table "rs_reputations", force: :cascade do |t|
-    t.string   "reputation_name", limit: 255
-    t.float    "value",                       default: 0.0
-    t.string   "aggregated_by",   limit: 255
-    t.integer  "target_id"
-    t.string   "target_type",     limit: 255
-    t.boolean  "active",                      default: true
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.text     "data"
-  end
-
-  add_index "rs_reputations", ["reputation_name", "target_id", "target_type"], name: "index_rs_reputations_on_reputation_name_and_target", unique: true
-  add_index "rs_reputations", ["reputation_name"], name: "index_rs_reputations_on_reputation_name"
-  add_index "rs_reputations", ["target_id", "target_type"], name: "index_rs_reputations_on_target_id_and_target_type"
-
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
