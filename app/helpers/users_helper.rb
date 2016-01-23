bf48afa4e978f2d9a035b16fa765c45290ef1579
ken1flan <ken1flan@gmail.com>
2016-01-23 19:16:19 +0900

    Re-annotate

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :integer          not null, primary key
+#  nickname     :string(255)
+#  club_id      :integer          default(1), not null
+#  admin        :boolean          default(FALSE), not null
+#  created_at   :datetime
+#  updated_at   :datetime
+#  icon_url     :string(255)
+#  belonging_to :string
+#
+# Indexes
+#
+#  index_users_on_club_id  (club_id)
+#
+
 module UsersHelper
 end
